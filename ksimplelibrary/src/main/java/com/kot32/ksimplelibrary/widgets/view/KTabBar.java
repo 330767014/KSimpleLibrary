b7e4c93e0fea1d0cdd7d8cfaeff7da1b1338543e
kot32 <kot32go@gmail.com>
2016-02-10 14:07:57 +0800

    Update KTabBar.java

--- ksimplelibrary/src/main/java/com/kot32/ksimplelibrary/widgets/view/KTabBar.java
+++ ksimplelibrary/src/main/java/com/kot32/ksimplelibrary/widgets/view/KTabBar.java
@@ -1,8 +1,8 @@
 package com.kot32.ksimplelibrary.widgets.view;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.support.percent.PercentLayoutHelper;
+import android.support.percent.PercentRelativeLayout;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -10,17 +10,23 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.kot32.ksimplelibrary.util.tools.ViewUtil;
+import com.kot32.ksimplelibrary.util.tools.reflect.FieldUtils;
 import com.kot32.ksimplelibrary.widgets.base.KBaseWidgets;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by kot32 on 15/11/15.
  * <p/>
- * 可定制的Tab界面 STYLE_GRADUAL 类似微信的渐变+可滑动的效果 STYLE_NORMAL 类似美团的点击变成选中+不可滑动效果
+ * 可定制的Tab界面
+ * STYLE_GRADUAL 类似微信的渐变+可滑动的效果
+ * STYLE_NORMAL 类似美团的点击变成选中+不可滑动效果
  */
 public class KTabBar extends KBaseWidgets {
 
@@ -28,21 +34,21 @@ public class KTabBar extends KBaseWidgets {
         STYLE_GRADUAL, STYLE_NORMAL
     }
 
-    private TabStyle           style     = TabStyle.STYLE_GRADUAL;
+    private TabStyle style = TabStyle.STYLE_GRADUAL;
 
-    private List<TabView>      tabs      = new ArrayList<>();
+    private List<TabView> tabs = new ArrayList<>();
 
-    private int                count     = 0;
-    private int                lastIndex = 0;
+    private int count = 0;
+    private int lastIndex = 0;
 
     private OnTabClickListener onTabClickListener;
 
-    public KTabBar(Context context, TabStyle style){
+    public KTabBar(Context context, TabStyle style) {
         super(context);
         this.style = style;
     }
 
-    public KTabBar(Context context, AttributeSet attrs){
+    public KTabBar(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -62,42 +68,30 @@ public class KTabBar extends KBaseWidgets {
 
     }
 
-    public void addTab(int imgId, int highlightImgId, String text, int fontColor, int highlightFontColor) {
-        Bitmap imgBitmap = BitmapFactory.decodeResource(getResources(), imgId);
-        Bitmap highlightImgBitmap = BitmapFactory.decodeResource(getResources(), highlightImgId);
-        addTab(imgBitmap, highlightImgBitmap, text, fontColor, highlightFontColor);
-    }
 
-    public void addTab(Bitmap imgBitmap, Bitmap highlightImgBitmap, String text, int fontColor, int highlightFontColor) {
+    public void addTab(int imgId, int highlightImgId, String text, int fontColor, int highlightFontColor, float widthRatio, float heightRatio, float marginTopRatio) {
         TabView tabView = null;
 
         if (style == TabStyle.STYLE_GRADUAL) {
-            tabView = new GradualTabView(getContext()).NewTabView(imgBitmap, highlightImgBitmap, text, fontColor,
-                                                                  highlightFontColor);
+            tabView = new GradualTabView(getContext()).NewTabView(imgId, highlightImgId, text, fontColor, highlightFontColor, widthRatio, heightRatio, marginTopRatio);
         } else if (style == TabStyle.STYLE_NORMAL) {
-            tabView = new NormalTabView(getContext()).NewTabView(imgBitmap, highlightImgBitmap, text, fontColor,
-                                                                 highlightFontColor);
-        } else {
-            tabView = new GradualTabView(getContext()).NewTabView(imgBitmap, highlightImgBitmap, text, fontColor,
-                                                                  highlightFontColor);
+            tabView = new NormalTabView(getContext()).NewTabView(imgId, highlightImgId, text, fontColor, highlightFontColor, widthRatio, heightRatio, marginTopRatio);
         }
-
         count++;
         final int index = count - 1;
         LayoutParams layoutParams = new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
-        layoutParams.setMargins(10, 10, 10, 10);
         tabView.setLayoutParams(layoutParams);
+
         this.addView(tabView);
         tabs.add(tabView);
 
         tabView.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 if (onTabClickListener != null) {
                     onTabClickListener.onClick(index);
                 }
-                // 重置其他所有TAB
+                //重置其他所有TAB
                 for (TabView tab : tabs) {
                     if (tab == v) continue;
                     tab.beDarker(1);
@@ -113,22 +107,23 @@ public class KTabBar extends KBaseWidgets {
 
     }
 
-    class TabView extends LinearLayout {
 
-        public TabView(Context context){
+    class TabView extends PercentRelativeLayout {
+
+        public TabView(Context context) {
             super(context);
         }
 
-        public TabView(Context context, AttributeSet attrs){
+        public TabView(Context context, AttributeSet attrs) {
             super(context, attrs);
         }
 
-        // 变为不选中
+        //变为不选中
         public void beDarker(float offset) {
 
         }
 
-        // 变为选中
+        //变为选中
         public void beLighter(float offset) {
 
         }
@@ -145,45 +140,37 @@ public class KTabBar extends KBaseWidgets {
     public class GradualTabView extends TabView {
 
         private FrameLayout imageSpace;
-        private ImageView   imageView_outer;
-        private ImageView   imageView_inner;
+        private ImageView imageView_outer;
+        private ImageView imageView_inner;
 
-        private TextView    textView_outer;
-        private TextView    textView_inner;
+        private TextView textView_outer;
+        private TextView textView_inner;
 
         private FrameLayout textSpace;
 
-        public GradualTabView(Context context){
+
+        public GradualTabView(Context context) {
             super(context);
             init();
         }
 
-        public GradualTabView(Context context, AttributeSet attrs){
+        public GradualTabView(Context context, AttributeSet attrs) {
             super(context, attrs);
             init();
         }
 
         private void init() {
-            this.setOrientation(VERTICAL);
+
             imageSpace = new FrameLayout(getContext());
             imageView_outer = new ImageView(getContext());
             imageView_inner = new ImageView(getContext());
+
 
             textView_inner = new TextView(getContext());
             textView_outer = new TextView(getContext());
         }
 
-        public GradualTabView NewTabView(int iconImgId, int highlightImgId, String text, int fontColor,
-                                         int highlightFontColor) {
-
-            Bitmap iconImgBitmap = BitmapFactory.decodeResource(getResources(), iconImgId);
-            Bitmap highlightImgBitmap = BitmapFactory.decodeResource(getResources(), highlightImgId);
-
-            return NewTabView(iconImgBitmap, highlightImgBitmap, text, fontColor, highlightFontColor);
-        }
-
-        public GradualTabView NewTabView(Bitmap iconImgBitmap, Bitmap highlightImgBitmap, String text, int fontColor,
-                                         int highlightFontColor) {
+        public GradualTabView NewTabView(int iconImgId, int highlightImgId, String text, int fontColor, int highlightFontColor, float wr, float hr, float tr) {
             textView_inner.setText(text);
             textView_inner.setTextColor(highlightFontColor);
             textView_inner.setTextSize(12);
@@ -194,27 +181,44 @@ public class KTabBar extends KBaseWidgets {
             textView_outer.setTextSize(12);
             textView_outer.setAlpha(1);
 
-            imageView_inner.setImageBitmap(highlightImgBitmap);
+            imageView_inner.setImageResource(highlightImgId);
             imageView_inner.setAlpha(0f);
-            imageView_outer.setImageBitmap(iconImgBitmap);
+            imageView_outer.setImageResource(iconImgId);
             imageView_outer.setAlpha(1f);
 
-            imageSpace.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1));
-            imageSpace.addView(imageView_inner, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                                                           ViewGroup.LayoutParams.MATCH_PARENT));
-            imageSpace.addView(imageView_outer, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                                                           ViewGroup.LayoutParams.MATCH_PARENT));
+            /**
+             * Google 竟然没提供set 方法。。罢了
+             */
+            PercentLayoutHelper.PercentLayoutInfo percentInfo = new PercentLayoutHelper.PercentLayoutInfo();
+            percentInfo.heightPercent = hr;
+            percentInfo.widthPercent = wr;
+            percentInfo.topMarginPercent = tr;
+            PercentRelativeLayout.LayoutParams percentLayoutParams = new PercentRelativeLayout.LayoutParams(mContext, null);
+            percentLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            //获取到LayoutInfo 并进行修改
+            Field infoField = FieldUtils.getDeclaredField(PercentRelativeLayout.LayoutParams.class, "mPercentLayoutInfo", true);
+            try {
+                FieldUtils.writeField(infoField, percentLayoutParams, percentInfo);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
 
-            LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                                                                                 ViewGroup.LayoutParams.WRAP_CONTENT);
-            textParams.gravity = Gravity.CENTER_HORIZONTAL;
+            int dyId = ViewUtil.generateViewId();
+            imageSpace.setId(dyId);
+            imageSpace.setLayoutParams(percentLayoutParams);
+            imageSpace.addView(imageView_inner, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            imageSpace.addView(imageView_outer, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
+
+            RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            textParams.addRule(RelativeLayout.BELOW, dyId);
+            textParams.addRule(RelativeLayout.CENTER_HORIZONTAL,TRUE);
             textSpace = new FrameLayout(getContext());
+
             textSpace.setLayoutParams(textParams);
-            textSpace.addView(textView_inner, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                                                         ViewGroup.LayoutParams.MATCH_PARENT));
-            textSpace.addView(textView_outer, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                                                         ViewGroup.LayoutParams.MATCH_PARENT));
+            textSpace.addView(textView_inner, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            textSpace.addView(textView_outer, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
 
             textView_inner.setGravity(Gravity.CENTER_HORIZONTAL);
             textView_outer.setGravity(Gravity.CENTER_HORIZONTAL);
@@ -224,7 +228,8 @@ public class KTabBar extends KBaseWidgets {
             return this;
         }
 
-        // 选中状态
+
+        //选中状态
         public void beLighter(float offset) {
 
             imageView_inner.setAlpha(offset);
@@ -235,6 +240,7 @@ public class KTabBar extends KBaseWidgets {
 
         }
 
+
         public void beDarker(float offset) {
 
             imageView_inner.setAlpha(1 - offset);
@@ -242,6 +248,7 @@ public class KTabBar extends KBaseWidgets {
 
             textView_inner.setAlpha(1 - offset);
             textView_outer.setAlpha(offset);
+
 
         }
 
@@ -253,38 +260,28 @@ public class KTabBar extends KBaseWidgets {
     class NormalTabView extends TabView {
 
         private ImageView imageView;
-        private TextView  textView;
-        private Bitmap    iconImgBitmap, highlightImgBitmap;
-        private int       fontColor, highlightFontColor;
+        private TextView textView;
+        private int iconImgId, highlightImgId;
+        private int fontColor, highlightFontColor;
 
-        public NormalTabView(Context context){
+        public NormalTabView(Context context) {
             super(context);
             init();
         }
 
-        public NormalTabView(Context context, AttributeSet attrs){
+        public NormalTabView(Context context, AttributeSet attrs) {
             super(context, attrs);
             init();
         }
 
         private void init() {
-            this.setOrientation(VERTICAL);
             imageView = new ImageView(getContext());
             textView = new TextView(getContext());
         }
 
-        public NormalTabView NewTabView(int iconImgId, int highlightImgId, String text, int fontColor,
-                                        int highlightFontColor) {
-            Bitmap iconImgBitmap = BitmapFactory.decodeResource(getResources(), iconImgId);
-            Bitmap highlightImgBitmap = BitmapFactory.decodeResource(getResources(), highlightImgId);
-
-            return NewTabView(iconImgBitmap, highlightImgBitmap, text, fontColor, highlightFontColor);
-        }
-
-        public NormalTabView NewTabView(Bitmap iconImgBitmap, Bitmap highlightImgBitmap, String text, int fontColor,
-                                        int highlightFontColor) {
-            this.iconImgBitmap = iconImgBitmap;
-            this.highlightImgBitmap = highlightImgBitmap;
+        public NormalTabView NewTabView(int iconImgId, int highlightImgId, String text, int fontColor, int highlightFontColor, float wr, float hr, float tr) {
+            this.iconImgId = iconImgId;
+            this.highlightImgId = highlightImgId;
             this.fontColor = fontColor;
             this.highlightFontColor = highlightFontColor;
 
@@ -292,10 +289,31 @@ public class KTabBar extends KBaseWidgets {
             textView.setTextColor(fontColor);
             textView.setTextSize(12);
 
-            imageView.setImageBitmap(iconImgBitmap);
-            imageView.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1));
+            imageView.setImageResource(iconImgId);
+
+            PercentLayoutHelper.PercentLayoutInfo percentInfo = new PercentLayoutHelper.PercentLayoutInfo();
+            percentInfo.heightPercent = hr;
+            percentInfo.widthPercent = wr;
+            percentInfo.topMarginPercent = tr;
+            PercentRelativeLayout.LayoutParams percentLayoutParams = new PercentRelativeLayout.LayoutParams(mContext, null);
+            percentLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            //获取到LayoutInfo 并进行修改
+            Field infoField = FieldUtils.getDeclaredField(PercentRelativeLayout.LayoutParams.class, "mPercentLayoutInfo", true);
+            try {
+                FieldUtils.writeField(infoField, percentLayoutParams, percentInfo);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            int dyId = ViewUtil.generateViewId();
+            imageView.setLayoutParams(percentLayoutParams);
+            imageView.setId(dyId);
 
             textView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+            RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            textParams.addRule(RelativeLayout.BELOW, dyId);
+            textParams.addRule(RelativeLayout.CENTER_HORIZONTAL,TRUE);
+            textView.setLayoutParams(textParams);
 
             this.addView(imageView);
             this.addView(textView);
@@ -305,22 +323,22 @@ public class KTabBar extends KBaseWidgets {
         @Override
         public void beDarker(float offset) {
 
-            imageView.setImageBitmap(iconImgBitmap);
+            imageView.setImageResource(this.iconImgId);
             textView.setTextColor(fontColor);
         }
 
         @Override
         public void beLighter(float offset) {
 
-            imageView.setImageBitmap(highlightImgBitmap);
+            imageView.setImageResource(this.highlightImgId);
             textView.setTextColor(highlightFontColor);
         }
     }
 
     public interface OnTabClickListener {
-
         void onClick(int index);
     }
+
 
     public void setOnTabClickListener(OnTabClickListener onTabClickListener) {
         this.onTabClickListener = onTabClickListener;
